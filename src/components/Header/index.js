a6779efc8de1b14b5524bebea657c9e896837fe7
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-01-21 22:16:57 -0300

    Estilizando notificações

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
 import logo from '~/assets/small-logo.svg';
 
 import { Container, Content, Profile } from './styles';
@@ -14,6 +16,7 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Ramon Mesquita</strong>
