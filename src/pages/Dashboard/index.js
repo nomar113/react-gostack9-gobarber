6cb8d0d78b05c92b097f90b8209fbd4826af8f92
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-01-22 04:28:14 -0300

    Página de livros

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Book } from './styles';
 
+import api from '~/services/api';
+
 export default function Dashboard() {
+  const [books, setBooks] = useState([]);
+  const [selectedBooks, setSelectedBooks] = useState([]);
+
+  useEffect(() => {
+    async function loadBooks() {
+      const response = await api.get('/books');
+
+      setBooks(response.data);
+    }
+    loadBooks();
+  }, [books]);
+
   return (
     <Container>
       <ul>
-        <Book>
-          <strong>Dom Casmurro</strong>
-          <span>Machado de Assis</span>
-          <span>Romance</span>
-        </Book>
-        <Book>
-          <strong>O Auto da Compadecida</strong>
-          <span>Ariano Suassuna</span>
-          <span>Comédia</span>
-        </Book>
-        <Book>
-          <strong>Clean Code</strong>
-          <span>Eric Evans</span>
-          <span>Tecnologia</span>
-        </Book>
-        <Book>
-          <strong>O Pequeno Príncipe</strong>
-          <span />
-          <span>Infantil</span>
-        </Book>
+        {books.map(book => (
+          <Book key={book._id}>
+            <strong>{book.title}</strong>
+            <span>{book.author}</span>
+            <span>{book.category}</span>
+          </Book>
+        ))}
       </ul>
     </Container>
   );
