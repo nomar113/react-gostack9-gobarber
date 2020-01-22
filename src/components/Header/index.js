28ad4cfce8adae11ca2fcc273ceb47a15fcb2230
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-01-22 01:06:54 -0300

    Dados do Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +8,8 @@ import logo from '~/assets/small-logo.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -19,7 +22,7 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Ramon Mesquita</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
